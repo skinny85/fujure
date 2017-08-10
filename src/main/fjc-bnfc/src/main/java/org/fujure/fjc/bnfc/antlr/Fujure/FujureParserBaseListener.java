ba581a675a8333563dd51861869f81e88bcbe0f1
Adam Ruka <adamruka85@gmail.com>
2017-08-09 20:04:54 -0700

    Change the fjc-bnfc project to be compiled by Gradle (allows for a much better IDE experience).

---
+++ src/main/fjc-bnfc/src/main/java/org/fujure/fjc/bnfc/antlr/Fujure/FujureParserBaseListener.java
@@ -0,0 +1,51 @@
+// Generated from org/fujure/fjc/bnfc/antlr/Fujure/FujureParser.g4 by ANTLR 4.7
+package org.fujure.fjc.bnfc.antlr.Fujure;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link FujureParserListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class FujureParserBaseListener implements FujureParserListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterVDef(FujureParser.VDefContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitVDef(FujureParser.VDefContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
