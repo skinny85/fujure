bd0d52dbee49905e0ca188010bd0c585921cd168
Adam Ruka <adamruka85@gmail.com>
2017-08-09 20:31:12 -0700

    Move the fujure-java-compiler to Kotlin.

--- src/main/fujure-java-compiler/src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-import org.fujure.fjc.bnfc.antlr.Fujure.Test;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Test.main(args);
-    }
-}
---
+++ src/main/fujure-java-compiler/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+import org.fujure.fjc.bnfc.antlr.Fujure.Test;
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Test.main(args);
+    }
+}
