bd4823fd137dc46f5b633c0f7b55a36820b5f161
Adam Ruka <adamruka85@gmail.com>
2017-08-12 17:10:32 -0700

    Refactor to improve the type safety of the `when` expressions in Main.

--- src/main/fujure-java-compiler/src/main/kotlin/org/fujure/fjc/Main.kt
+++ src/main/fujure-java-compiler/src/main/kotlin/org/fujure/fjc/Main.kt
@@ -31,43 +31,47 @@ object Main {
     }
 
     private fun openFiles(vararg files: String): List<ArgumentFile.OpenedFile> {
-        val openFiles = mutableListOf<ArgumentFile.OpenedFile>()
+        val openFiles = mutableListOf<ArgumentFile.OpenedFile?>()
         for (file in files) {
             val tryOpenFile = ArgumentFile.openFile(file)
-            when (tryOpenFile) {
+            openFiles.add(when (tryOpenFile) {
                 is ArgumentFile.InvalidFilename -> {
                     println("Invalid file name: $file. Fujure source files must have the .fjr extension")
+                    null
                 }
                 is ArgumentFile.MissingFile -> {
                     println("Error opening $file: file not found")
+                    null
                 }
                 is ArgumentFile.FailedFile -> {
                     println("Error opening $file: ${tryOpenFile.error.message}")
+                    null
                 }
                 is ArgumentFile.OpenedFile -> {
-                    openFiles.add(tryOpenFile)
+                    tryOpenFile
                 }
-            }
+            })
         }
-        return openFiles
+        return openFiles.filterNotNull()
     }
 
     private fun parseFiles(openFiles: List<ArgumentFile.OpenedFile>): List<ReadFile.ParsedFile> {
-        val parsedFiles = mutableListOf<ReadFile.ParsedFile>()
+        val parsedFiles = mutableListOf<ReadFile.ParsedFile?>()
         for (openFile in openFiles) {
             val tryParseFile = ReadFile.parse(openFile)
-            when (tryParseFile) {
+            parsedFiles.add(when (tryParseFile) {
                 is ReadFile.UnparsedFile -> {
                     println("Couldn't parse ${tryParseFile.userProvidedFile}:")
                     for ((line, column, msg) in tryParseFile.errors) {
                         println("${tryParseFile.userProvidedFile}:${line},${column}: ${msg}")
                     }
+                    null
                 }
                 is ReadFile.ParsedFile -> {
-                    parsedFiles.add(tryParseFile)
+                    tryParseFile
                 }
-            }
+            })
         }
-        return parsedFiles
+        return parsedFiles.filterNotNull()
     }
 }
