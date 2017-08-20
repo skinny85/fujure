d4a7ca762c63220c4ca92201e818baeb5ce9dd5f
Adam Ruka <adamruka85@gmail.com>
2017-08-19 23:11:13 -0700

    Changed the Gradle plugin to use Java plugin's source sets (the task is now compileFujure).

--- src/tools/fujure-gradle-plugin/src/main/groovy/org/fujure/gradle/FujureGradlePlugin.groovy
+++ src/tools/fujure-gradle-plugin/src/main/groovy/org/fujure/gradle/FujureGradlePlugin.groovy
@@ -2,9 +2,24 @@ package org.fujure.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaBasePlugin
+import org.gradle.api.plugins.JavaPlugin
+import org.gradle.api.plugins.JavaPluginConvention
 
 class FujureGradlePlugin implements Plugin<Project> {
     void apply(Project project) {
-        project.tasks.create('fujure', CompileFujureTask)
+        project.pluginManager.apply(JavaBasePlugin)
+
+        configureSourceSetDefaults(project)
+
+        project.pluginManager.apply(JavaPlugin)
+    }
+
+    private void configureSourceSetDefaults(Project project) {
+        project.convention.getPlugin(JavaPluginConvention).sourceSets.all { sourceSet ->
+            def taskName = sourceSet.getCompileTaskName('fujure')
+            def task = project.tasks.create(taskName, CompileFujureTask)
+            task.description = "Compiles ${sourceSet.name} Fujure source."
+        }
     }
 }
