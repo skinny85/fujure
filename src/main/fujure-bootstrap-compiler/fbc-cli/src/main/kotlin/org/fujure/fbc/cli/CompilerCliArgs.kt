3e59dfeddeb332042618a0ba1b536144c92e5271
Adam Ruka <adamruka85@gmail.com>
2017-09-03 22:35:15 -0700

    Remove unused methods from the CompilerCliArgs class.

--- src/main/fujure-bootstrap-compiler/fbc-cli/src/main/kotlin/org/fujure/fbc/cli/CompilerCliArgs.kt
+++ src/main/fujure-bootstrap-compiler/fbc-cli/src/main/kotlin/org/fujure/fbc/cli/CompilerCliArgs.kt
@@ -12,22 +12,4 @@ class CompilerCliArgs(parser: ArgParser) {
     val sourceFiles by parser
             .positionalList("SOURCE",
                     help = "Source files (with the .fjr extension) to compile")
-
-    fun printHelp() {
-        println(USAGE_STRING)
-        println("\nOptions:")
-        println("""
-                |   -o, --output = OUTPUT_DIR    The directory that the output files should be generated to
-                |   -h, --help                   Print this help and exit
-                |""".trimMargin())
-    }
-
-    fun printUsageAndHelpTip() {
-        println(CompilerCliArgs.USAGE_STRING)
-        println("Use the -h (or --help) option for more information")
-    }
-
-    companion object {
-        private val USAGE_STRING = "Usage: fbc <options> file1.fjr file2.fjr ..."
-    }
 }
