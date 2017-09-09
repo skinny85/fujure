97679e756ce1bd17432d3c65dc3310a22303ff24
Adam Ruka <adamruka85@gmail.com>
2017-09-09 00:34:57 -0700

    API classes for the second pass of semantic analysis (correctness verification).

---
+++ src/main/fujure-bootstrap-compiler/fbc-core/src/main/kotlin/org/fujure/fbc/analyze/pass_02/VerificationAnalysis.kt
@@ -0,0 +1,11 @@
+package org.fujure.fbc.analyze.pass_02
+
+import org.fujure.fbc.analyze.SemanticAnalysisResult
+import org.fujure.fbc.ast.SymbolTable
+import org.fujure.fbc.parse.ParsedFile
+
+object VerificationAnalysis {
+    fun analyze(parsedFiles: List<ParsedFile>, symbolTable: SymbolTable): SemanticAnalysisResult {
+        throw UnsupportedOperationException()
+    }
+}
