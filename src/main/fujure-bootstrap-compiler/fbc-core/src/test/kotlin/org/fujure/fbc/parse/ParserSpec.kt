cbcdf4e48545603a6700eb2c0228f34e8f5ef858
Adam Ruka <adamruka85@gmail.com>
2017-12-13 19:01:52 -0800

    Refactor the ParserSpec.

--- src/main/fujure-bootstrap-compiler/fbc-core/src/test/kotlin/org/fujure/fbc/parse/ParserSpec.kt
+++ src/main/fujure-bootstrap-compiler/fbc-core/src/test/kotlin/org/fujure/fbc/parse/ParserSpec.kt
@@ -7,153 +7,165 @@ import org.fujure.fbc.read.OpenedFile
 import org.fujure.test.utils.Assumption.Companion.assume
 import org.specnaz.kotlin.junit.SpecnazKotlinJUnit
 
-class ParserSpec : SpecnazKotlinJUnit("Parser", {
+class ParserSpec : SpecnazKotlinJUnit("Parser#parse", {
     val parser: Parser = BnfcParser
     lateinit var result: ParsingResult
 
+    fun parse(program: String) {
+        result = parser.parse(OpenedFile(InputFile("whatever.fjr"), ANTLRInputStream(program)))
+    }
+
+    fun assertParsingFailed() {
+        val parsingFailure = assume(result).isA<ParsingResult.Failure>()
+        assertThat(parsingFailure.cause.errors).isNotEmpty
+    }
+
+    fun assertParsingSucceeded() {
+        assume(result).isA<ParsingResult.Success>()
+    }
+
     it.describes("called with a file containing 'def xxx'") {
         it.beginsAll {
-            result = parser.parse(OpenedFile(InputFile("whatever.fjr"), ANTLRInputStream("""
+            parse("""
                 def xxx
-                """)))
+                """)
         }
 
-        it.should("return a ParsingResult\$Failure") {
-            val parsingFailure = assume(result).isA<ParsingResult.Failure>()
-            assertThat(parsingFailure.cause.errors).isNotEmpty()
+        it.should("fail parsing") {
+            assertParsingFailed()
         }
     }
 
     it.describes("called with a file containing just a hash") {
         it.beginsAll {
-            result = parser.parse(OpenedFile(InputFile("whatever.fjr"), ANTLRInputStream("""
+            parse("""
                 #
-                """)))
+                """)
         }
 
         it.should("fail parsing") {
-            assume(result).isA<ParsingResult.Failure>()
+            assertParsingFailed()
         }
     }
 
     it.describes("called with a value with the name 'package'") {
         it.beginsAll {
-            result = parser.parse(OpenedFile(InputFile("whatever.fjr"), ANTLRInputStream("""
+            parse("""
                 def package = 1
-                """)))
+                """)
         }
 
         it.should("fail parsing") {
-            val parsingFailure = assume(result).isA<ParsingResult.Failure>()
-            assertThat(parsingFailure.cause.errors).isNotEmpty()
+            assertParsingFailed()
         }
     }
 
     it.describes("called with a Char value") {
         it.beginsAll {
-            result = parser.parse(OpenedFile(InputFile("whatever.fjr"), ANTLRInputStream("""
+            parse("""
                 def c = 'a'
-                """)))
+                """)
         }
 
-        it.should("return a ParsingResult\$Success") {
-            assume(result).isA<ParsingResult.Success>()
+        it.should("parse the program correctly") {
+            assertParsingSucceeded()
         }
     }
 
     it.describes("called with correctly escaped Char values") {
         it.beginsAll {
-            result = parser.parse(OpenedFile(InputFile("whatever.fjr"), ANTLRInputStream("""
+            parse("""
                 def c1 = '\''
                 def c2 = '\\'
-                """)))
+                """)
         }
 
-        it.should("return a ParsingResult\$Success") {
-            assume(result).isA<ParsingResult.Success>()
+        it.should("parse the program correctly") {
+            assertParsingSucceeded()
         }
     }
 
     it.describes("called with an unescaped backslash in a Char value") {
         it.beginsAll {
-            result = parser.parse(OpenedFile(InputFile("whatever.fjr"), ANTLRInputStream("""
+            parse("""
                 def c = '\'
-                """)))
+                """)
         }
 
         it.should("fail parsing") {
-            assume(result).isA<ParsingResult.Failure>()
+            assertParsingFailed()
         }
     }
 
     it.describes("called with an unescaped quote in a Char value") {
         it.beginsAll {
-            result = parser.parse(OpenedFile(InputFile("whatever.fjr"), ANTLRInputStream("""
+            parse("""
                 def c = '''
-                """)))
+                """)
         }
 
         it.should("fail parsing") {
-            assume(result).isA<ParsingResult.Failure>()
+            assertParsingFailed()
         }
     }
 
     it.describes("called with an empty Char literal value") {
         it.beginsAll {
-            result = parser.parse(OpenedFile(InputFile("whatever.fjr"), ANTLRInputStream("""
+            parse("""
                 def c = ''
-                """)))
+                """)
         }
 
         it.should("fail parsing") {
-            assume(result).isA<ParsingResult.Failure>()
+            assertParsingFailed()
         }
     }
 
     it.describes("called with a String value") {
         it.beginsAll {
-            result = parser.parse(OpenedFile(InputFile("whatever.fjr"), ANTLRInputStream("""
+            parse("""
                 def c = "a"
-                """)))
+                """)
         }
 
-        it.should("return a ParsingResult\$Success") {
-            assume(result).isA<ParsingResult.Success>()
+        it.should("parse the program correctly") {
+            assertParsingSucceeded()
         }
     }
 
     it.describes("called with a String value containing some escaped characters") {
         it.beginsAll {
-            result = parser.parse(OpenedFile(InputFile("whatever.fjr"), ANTLRInputStream("""
+            parse("""
                 def c = "\n\\"
-                """)))
+                """)
         }
 
-        it.should("return a ParsingResult\$Success") {
-            assume(result).isA<ParsingResult.Success>()
+        it.should("parse the program correctly") {
+            assertParsingSucceeded()
         }
     }
 
     it.describes("called with a String value containing only a backslash") {
         it.beginsAll {
-            result = parser.parse(OpenedFile(InputFile("whatever.fjr"), ANTLRInputStream("""
+            parse("""
                 def s = "\"
-                """)))
+                """)
         }
 
-        it.should("return a ParsingResult\$Failure") {
-            assume(result).isA<ParsingResult.Failure>()
+        it.should("fail parsing") {
+            assertParsingFailed()
         }
     }
 
     it.describes("called with a String value containing only a double quote") {
         it.beginsAll {
-            result = parser.parse(OpenedFile(InputFile("whatever.fjr"),
-                    ANTLRInputStream("def s = \"\"\"")))
+            parse(
+                    "def s = \"\"\""
+            )
         }
 
         it.should("fail parsing") {
-            assume(result).isA<ParsingResult.Failure>()
+            assertParsingFailed()
         }
     }
 })
