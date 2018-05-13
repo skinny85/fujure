a86c72b9b005b8be1799018a9bfbcbdd1fb493ef
Adam Ruka <adamruka85@gmail.com>
2018-05-13 12:15:14 -0700

    Moved the Truffle JS Spec to Kotlin.

---
+++ src/main/truffle/fujure-truffle/src/test/kotlin/TruffleJsKotlinSpec.kt
@@ -0,0 +1,11 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.graalvm.polyglot.Context
+import org.specnaz.kotlin.junit.SpecnazKotlinJUnit
+
+class TruffleJsKotlinSpec : SpecnazKotlinJUnit("Using Truffle Context", {
+    it.should("successfully evaluate some JS code") {
+        val context = Context.create();
+        val value = context.eval("js", "1 + 2");
+        assertThat(value.asInt()).isEqualTo(3);
+    }
+})
