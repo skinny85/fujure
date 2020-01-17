e7435721e9bfa404f864751c53ca886e4321a94f
Adam Ruka <adamruka85@gmail.com>
2020-01-16 22:15:42 -0800

    Add some examples of forward references in Java, for fields and methods.

--- src/main/fujure-bootstrap-compiler/fbc-core/src/test/java/a/b/c/Y.java
+++ src/main/fujure-bootstrap-compiler/fbc-core/src/test/java/a/b/c/Y.java
@@ -1,5 +1,7 @@
 package a.b.c;
 
+import java.util.function.Function;
+
 /**
  * A class used in {@link a.Main} to test importing classes
  * with the same name.
@@ -7,6 +9,16 @@ package a.b.c;
  * @see a.b.Y
  */
 public class Y {
+//    private static int j = i; // this is not allowed: "Illegal forward reference"
+
     // don't need an explicit import to use a class from the java.lang package
     public static final int i = Integer.MAX_VALUE - 1;
+
+    // this works fine
+    public static boolean odd(int n) { return n <= 0 ? false : even(n - 1); }
+    public static boolean even(int n) { return n <= 0 ? true : odd(n - 1); }
+
+    // this doesn't work either!
+//    private static final Function<Integer, Boolean> lambdaOdd = (Integer n) -> n <= 0 ? false : lambdaEven.apply(n - 1);
+//    private static final Function<Integer, Boolean> lambdaEven = (Integer n) -> n <= 0 ? true : lambdaOdd.apply(n - 1);
 }
